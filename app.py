3f7e9adcc1a70ed1735708cb065f59fdfe31593b
NilReboot <EfilSiCisum@gmail.com>
2023-09-20 22:44:43 -0400

    inital repo created

---
+++ app.py
@@ -0,0 +1,167 @@
+import requests
+import sqlite3
+from datetime import datetime, timedelta
+import hashlib
+from typing import Optional, Tuple, List, Dict
+import argparse
+from contextlib import closing
+
+def create_tables(cursor: sqlite3.Cursor) -> None:
+    create_website_table_query = '''
+    CREATE TABLE IF NOT EXISTS websites (
+        url TEXT PRIMARY KEY,
+        timestamp DATETIME,
+        content_hash TEXT,
+        content TEXT
+    )
+    '''
+    create_urls_table_query = '''
+    CREATE TABLE IF NOT EXISTS urls (
+        url TEXT PRIMARY KEY
+    )
+    '''
+    create_website_archive_table_query = '''
+    CREATE TABLE IF NOT EXISTS website_archive (
+        url TEXT,
+        timestamp DATETIME,
+        content_hash TEXT,
+        content TEXT
+    )
+    '''
+    cursor.execute(create_website_table_query)
+    cursor.execute(create_website_archive_table_query)
+    cursor.execute(create_urls_table_query)
+
+def update_urls(cursor: sqlite3.Cursor, new_urls: List[str]) -> None:
+    timestamp = datetime.now()
+    for url in new_urls:
+        try:
+            cursor.execute('INSERT OR IGNORE INTO urls (url, last_checked) VALUES (?, ?)', (url, timestamp))
+            cursor.execute('UPDATE urls SET last_checked = ? WHERE url = ?', (timestamp, url))
+        except sqlite3.Error as e:
+            print(f"Database error occurred while updating URL {url}: {e}")
+    print(f"Added {len(new_urls)} url(s).")
+
+
+def remove_urls(cursor: sqlite3.Cursor, del_urls: List[str]) -> None:
+    del_cnt = 0
+    for url in del_urls:
+        try:
+            cursor.execute('DELETE FROM urls WHERE url = ?', (url,))
+            del_cnt += 1
+        except sqlite3.Error as e:
+            print(f"Database error occurred while deleting URL {url}: {e}")
+    print(f"Deleted {del_cnt} url(s).")
+
+def get_all_urls(cursor: sqlite3.Cursor) -> List[str]:
+    cursor.execute('SELECT url FROM urls')
+    return [row[0] for row in cursor.fetchall()]
+
+def get_website_content(url: str) -> str:
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        print(f"Request error occurred with {url}:", err)
+        return ''
+    return response.text
+
+def get_content_hash(content: str) -> str:
+    return hashlib.sha256(content.encode()).hexdigest()
+
+def should_fetch_content(cursor: sqlite3.Cursor, url: str, time_delta_minutes: int) -> Tuple[bool, Optional[str]]:
+    cursor.execute('SELECT content_hash FROM websites WHERE url = ?', (url,))
+    hash_result = cursor.fetchone()
+    cursor.execute('SELECT last_checked FROM urls WHERE url = ?', (url,))
+    timestamp = cursor.fetchone()[0]
+
+    if hash_result is not None:
+        old_content_hash = hash_result[0]
+    else:
+        return True, None
+
+    datetime_last_checked = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
+    timestamp_now_minus_window = datetime.now() - timedelta(minutes=time_delta_minutes)
+    if datetime_last_checked < timestamp_now_minus_window:
+        # print(f'URL {url} has not been checked in {time_delta_minutes} minutes.')
+        return True, old_content_hash
+    else:
+        print(f'URL {url} has been checked in last {time_delta_minutes} minutes.')
+
+    return False, None
+
+def store_website_content(cursor: sqlite3.Cursor, url: str, content: str) -> None:
+    timestamp = datetime.now()
+    content_hash = get_content_hash(content)
+
+    cursor.execute('INSERT INTO websites (url, timestamp, content_hash, content) VALUES (?, ?, ?, ?)', (url, timestamp, content_hash, content))
+
+def archive_old_website_content(cursor: sqlite3.Cursor, url: str) -> None:
+
+    cursor.execute('SELECT * FROM websites WHERE url = ?', (url,))
+    data = cursor.fetchone()
+
+    assert data is not None, "Attempted to archive old record, but couldn't find data to archive."
+
+    cursor.execute('INSERT INTO website_archive (url, timestamp, content_hash, content) VALUES (?, ?, ?, ?)', data)
+    cursor.execute('DELETE FROM websites WHERE url = ?', (url,))
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Monitor changes in websites.')
+    parser.add_argument('--new_urls', nargs='+', default=[],
+                        help='List of new URLs to monitor.')
+    parser.add_argument('--del_urls', nargs='+', default=[],
+                        help='List of URLs to stop monitoring.')
+    parser.add_argument('--time_delta_minutes', type=int, default=60,
+                        help='Time window in minutes for checking updates.')
+    return parser.parse_args()
+
+def monitor_website_changes(cursor: sqlite3.Cursor, urls: List[str], time_delta_minutes: int) -> Dict[str, int]:
+    stats = {'num_errors': 0, 'num_fetches': 0, 'num_changes': 0, 'num_new_pages': 0}
+
+    for url in urls:
+        should_fetch, old_content_hash = should_fetch_content(cursor, url, time_delta_minutes)
+        if should_fetch:
+            stats['num_fetches'] += 1
+            print(f'Evaluating URL: {url}')
+            current_content = get_website_content(url)
+            if not current_content:
+                stats['num_errors'] += 1
+                continue
+            current_content_hash = get_content_hash(current_content)
+            if old_content_hash is None:
+                print(f'Adding new page: {url}')
+                stats['num_new_pages'] += 1
+                store_website_content(cursor, url, current_content)
+            elif old_content_hash != current_content_hash:
+                print(f'Page has changed: {url}')
+                stats['num_changes'] += 1
+                archive_old_website_content(cursor, url)
+                store_website_content(cursor, url, current_content)
+            cursor.execute('UPDATE urls SET last_checked = ? WHERE url = ?', (datetime.now(), url))
+
+    return stats
+
+
+def main() -> None:
+    args = parse_args()
+
+    with sqlite3.connect('website_content.db') as conn, closing(conn.cursor()) as cursor:
+        create_tables(cursor)
+        update_urls(cursor, args.new_urls)
+        remove_urls(cursor, args.del_urls)
+        urls = get_all_urls(cursor)
+
+        print(f'Number of URLs to evaluate: {len(urls)}')
+
+        stats = monitor_website_changes(cursor, urls, args.time_delta_minutes)
+        
+        conn.commit()
+
+    if stats['num_fetches'] > 0:
+        print(f'Total new pages added: {stats["num_new_pages"]}')
+        print(f'Total pages changed: {stats["num_changes"]}')
+        print(f'Total errors: {stats["num_errors"]}')
+
+if __name__ == '__main__':
+    main()
---
+++ test_app.py
@@ -0,0 +1,61 @@
+import unittest
+import sqlite3
+from app import create_tables, update_urls, remove_urls, get_all_urls, should_fetch_content, get_website_content, get_content_hash, store_website_content, archive_old_website_content
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.conn = sqlite3.connect(':memory:')
+        self.cursor = self.conn.cursor()
+        create_tables(self.cursor)
+
+    def tearDown(self):
+        self.conn.close()
+
+    def test_update_urls(self):
+        update_urls(self.cursor, ['http://example.com'])
+        self.cursor.execute('SELECT * FROM urls')
+        result = self.cursor.fetchone()
+        self.assertEqual(result[0], 'http://example.com')
+
+    def test_remove_urls(self):
+        update_urls(self.cursor, ['http://example.com'])
+        remove_urls(self.cursor, ['http://example.com'])
+        self.cursor.execute('SELECT * FROM urls')
+        result = self.cursor.fetchone()
+        self.assertIsNone(result)
+
+    def test_get_all_urls(self):
+        update_urls(self.cursor, ['http://example.com'])
+        result = get_all_urls(self.cursor)
+        self.assertEqual(result, ['http://example.com'])
+
+    def test_should_fetch_content(self):
+        store_website_content(self.cursor, 'http://example.com', 'old content')
+        result = should_fetch_content(self.cursor, 'http://example.com', 60)
+        self.assertEqual(result, (True, get_content_hash('old content')))
+
+    def test_get_website_content(self):
+        result = get_website_content('http://example.com')
+        self.assertIsNotNone(result)
+
+    def test_get_content_hash(self):
+        result = get_content_hash('example content')
+        self.assertEqual(result, 'a9c4a6f9f8d6d7eae8f7f1c9e5d9d5d9')
+
+    def test_store_website_content(self):
+        store_website_content(self.cursor, 'http://example.com', 'example content')
+        self.cursor.execute('SELECT * FROM website_content')
+        result = self.cursor.fetchone()
+        self.assertEqual(result[0], 'http://example.com')
+        self.assertEqual(result[1], 'example content')
+
+    def test_archive_old_website_content(self):
+        store_website_content(self.cursor, 'http://example.com', 'old content')
+        archive_old_website_content(self.cursor, 'http://example.com')
+        self.cursor.execute('SELECT * FROM website_content_archive')
+        result = self.cursor.fetchone()
+        self.assertEqual(result[0], 'http://example.com')
+        self.assertEqual(result[1], 'old content')
+
+if __name__ == '__main__':
+    unittest.main()
